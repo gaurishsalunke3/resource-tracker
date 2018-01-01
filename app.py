81b85a4cfc12b03a1ea50f51a71c358abf9e135d
gaurishsalunke3 <gaurisharya@gmail.com>
2018-01-01 23:13:05 +0530

    UserModel and User resource file created. Authentication API created.

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from authenticate import authenticate_user, identity
+from resources.user import UserRegister
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employee_data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "salunke"
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate_user, identity)
+
+# api.add_resource(UserLogin, '/authenticate')
+
+if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
---
+++ models/user.py
@@ -0,0 +1,28 @@
+from db import db
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+    profile = db.Column(db.String(50))
+
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+
+    def register_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    def find_by_user_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
